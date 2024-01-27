5b85d55b3dd6a5f08fe40c67076f2b5013c40d74
Ian Marshall <ian.js.tutor@gmail.com>
2024-01-26 21:42:34 -0500

    lesson 4

--- _04_snow/snow.js
+++ _04_snow/snow.js
@@ -4,9 +4,11 @@ import { lerp } from "./lib.js";
 //settings
 const numParticles = 250;
 const minRadius = 5;
-const maxRadius = 50;
-const minSpeed = 4;
-const maxSpeed = 15;
+const maxRadius = 100;
+const minVx = -3;
+const maxVx = 3;
+const minVy = 2;
+const maxVy = 25;
 
 //state
 const particles = [];
@@ -16,17 +18,18 @@ function setupParticles(canvas) {
     for (let i=0; i<numParticles; i++) {
         particles[i] = getParticle(canvas);
     }
-    console.log(particles);
 }
 
 function getParticle({ width, height }) {
     const r = lerp(minRadius, maxRadius, Math.random());
+    const norm = r/maxRadius;
     return {
         x: lerp(0, width, Math.random()),
         y: lerp(-height, -r, Math.random()),
         r,
-        vx: 0,
-        vy: lerp(minSpeed, maxSpeed, r/maxRadius)
+        vx: lerp(minVx, maxVx, Math.random()),
+        vy: lerp(minVy, maxVy, norm),
+        a: 1 - norm
     };
 }
 
@@ -46,10 +49,13 @@ export function update(canvas) {
 }
 
 export function draw(ctx) {
-    ctx.fillStyle = "#ffffff55";
-    for (let { x, y, r } of particles) {
+    ctx.save();
+    ctx.fillStyle = "#ffffff";
+    for (let { x, y, r, a } of particles) {
+        ctx.globalAlpha = a;
         ctx.beginPath();
         ctx.arc(x, y, r, 0, Math.PI*2);
         ctx.fill();
     }
+    ctx.restore();
 }
