e1149d0f543cf0f56dc6472d0f5239ed86b24b99
Ian Marshall <ian.js.tutor@gmail.com>
2024-01-27 13:30:36 -0500

    warpdrive code

--- _04b_opt_warpdrive/warpdrive.js
+++ _04b_opt_warpdrive/warpdrive.js
@@ -3,13 +3,12 @@ import { lerp } from "./lib.js";
 
 //settings
 const numParticles = 50;
-const minRadius = 10;
-const maxRadius = 150;
-const minVx = -1;
-const maxVx = 1;
-const minVy = 0.25;
-const maxVy = 1;
-const color = "orangered";
+const minRadius = 2;
+const maxRadius = 5;
+const minSpeed = -1;
+const maxSpeed = 1;
+const acceleration = 1.05;
+const opacity = 0.03;
 
 //state
 const particles = [];
@@ -22,14 +21,13 @@ function setupParticles(canvas) {
 }
 
 function getParticle({ width, height }) {
-    const r = lerp(minRadius, maxRadius, Math.random());
     return {
-        x: lerp(r, width - r, Math.random()),
-        y: lerp(r, height - r, Math.random()),
-        r,
-        vx: lerp(minVx, maxVx, Math.random()),
-        vy: lerp(minVy, maxVy, r/maxRadius),
-        color
+        x: width/2,
+        y: height/2,
+        r: lerp(minRadius, maxRadius, Math.random()),
+        vx: lerp(minSpeed, maxSpeed, Math.random()),
+        vy: lerp(minSpeed, maxSpeed, Math.random()),
+        opacity
     };
 }
 
@@ -41,33 +39,27 @@ export function update(canvas) {
     const { width, height } = canvas;
     for (let i=0; i<particles.length; i++) {
         let p = particles[i];
-        //move
+        //move and accelerate, change opacity
         p.x += p.vx;
         p.y += p.vy;
-        //pong bounce?
-        if (p.x < p.r) {
-            p.x = p.r;
-            p.vx *= -1;
-        }
-        else if (p.x > width - p.r) {
-            p.x = width - p.r;
-            p.vx *= -1;
-        }
-        if (p.y < p.r) {
-            p.y = p.r;
-            p.vy *= -1;
-        }
-        else if (p.y > height - p.r) {
-            p.y = height - p.r;
-            p.vy *= -1;
-        }
+        p.vx *= acceleration;
+        p.vy *= acceleration;
+        p.opacity = Math.min(1, p.opacity * acceleration);
+        //off screen?
+        if (
+            p.x + p.r < 0 ||
+            p.x - p.r > width ||
+            p.y + p.r < 0 ||
+            p.y - p.r > height
+        ) particles[i] = getParticle(canvas);
     }
 }
 
 export function draw(ctx) {
     ctx.save();
-    for (let { x, y, r, color } of particles) {
-        ctx.fillStyle = color;
+    for (let { x, y, r, opacity } of particles) {
+        ctx.globalAlpha = opacity;
+        ctx.fillStyle = "white";
         ctx.beginPath();
         ctx.arc(x, y, r, 0, Math.PI*2);
         ctx.fill();
